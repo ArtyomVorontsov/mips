a05e6ef784b63d6058cd32d7cd6cc87f2ce5255f
Artjoms <artv20022@gmail.com>
2022-03-27 14:31:05 +0300

    Exercise 3 fixed

--- chapter19/exercise3.s
+++ chapter19/exercise3.s
@@ -1,4 +1,4 @@
-# perfect number searcher
+# perfect number verifier
 
 .text
 main:
@@ -6,49 +6,42 @@ main:
 lui $8, 0x1000		# base memory address
 ori $9, $0, 0		# init divider
 ori $10, $0, 0		# init accumulator
-ori $11, $0, 0		# divided number
-ori $7, $0, 5		# potencially perfect number
-ori $13, $0, 0		# memory offset  
-ori $14, $0, 100		# perfect number limit
+ori $11, $0, 0		# reminder
+ori $7, $0, 0		# potencial perfect number
+ori $13, $0, 500		# limit
 
-searcher_loop:
+external_loop:
 
-addiu $7, $7, 1		# increment potencially perfect number
+addiu $7, $7, 1
 
 loop:
 addiu $9, $9, 1		# increment divider
 div $7, $9
-mfhi $11
+mfhi $11		# load reminder
 bne $11, $0, loop
 sll $0, $0, $0
 addu $10, $10, $9	# add divider to accumulator
-beq $10, $7, loop_end
-sll $0, $0, $0
-slt $12, $10, $7
-bne $12, $0, loop
+bne $9, $7, loop
 sll $0, $0, $0
 loop_end:
 
-bne $10, $7, check_end
+sub $10, $10, $7
+bne $10, $7, end
 sll $0, $0, $0
-addu $8, $8, $13
-sw $7, ($8)
-addiu $13, $13, 4
+sw $7, ($8)		# store perfect number in memory location
+addiu $8, $8, 4
 
-check_end:
-sll $0, $0, $0
-
-ori $9, $0, 0		# drop to default
-ori $10, $0, 0
-ori $11, $0, 0
-
-slt $12, $7, $14	# set $12 to 1 if value in $7 is less than in $14, otherwise set 0
-bne $12, $0, searcher_loop
+end:
 sll $0, $0, $0
 
-searcher_loop_end:
+# clear divider and accumulator registers
+or $9, $0, $0
+or $10, $0, $0
+
+bne $13, $7, external_loop
 sll $0, $0, $0
- 
+external_loop_end:
 
 .data
 array: .space 800
+
