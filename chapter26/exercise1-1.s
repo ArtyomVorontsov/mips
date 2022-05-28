32a41d15a5e4c6a17b545f9a3fc8544e2dd4a8b6
Artjoms <artv20022@gmail.com>
2022-05-28 23:27:44 +0300

    Exercise 1 extended

--- chapter26/exercise1-1.s
+++ chapter26/exercise1-1.s
@@ -8,6 +8,13 @@
 
 .globl main
 main:
+
+loop:
+
+li $s0, 0	# clear register result
+li $s1, 0	# clear register temp1
+li $s2, 0	# clear register temp2 	
+
 jal prompt_num	# ask user for u
 nop
 move $s1, $v0
@@ -16,9 +23,16 @@ mul $s0, $s1, 5		# 5u
 mul $s0, $s0, $s0	# 5u^2
 mul $s1, $s1, 12	# 12u
 
+
 jal prompt_num	# ask user for v
 nop
 move $s2, $v0
+
+bne $s2, $zero, v_is_no_zero
+nop
+beq $s2, $s0, loop_end
+nop
+v_is_no_zero:
 
 # evaluate experession
 mul $s1, $s1, $s2	# 12uv
@@ -31,6 +45,10 @@ addu $s0, $s0, $s2	# 5u^2 - 12uv + 6v^2
 li $v0, 1	# print result 
 move $a0, $s0
 syscall
+
+j loop
+nop
+loop_end:
 
 li $v0, 10	# exit
 syscall
