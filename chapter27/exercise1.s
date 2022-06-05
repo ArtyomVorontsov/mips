c1af8fd869e87f86684a867bacf0439142199f5a
Artjoms <artv20022@gmail.com>
2022-06-05 13:21:38 +0300

    Chapter 27, Exercise 1 completed!

---
+++ chapter27/exercise1.s
@@ -0,0 +1,140 @@
+# Exercise 1 — Same String
+
+.text
+.globl main
+main:
+
+# prolog
+nop
+subu $sp, $sp, 4	# push $ra on stack
+sw $ra, ($sp)
+nop
+
+# code 
+# ask for string 1
+la $a0, buffer1
+li $a1, 100
+jal ask_for_string
+nop
+
+# ask for string 2
+la $a0, buffer2
+li $a1, 100
+jal ask_for_string
+nop
+
+# compare strings 
+la $a0, buffer1
+la $a1, buffer2 
+jal compare_strings
+nop
+
+move $t0, $v0		# move result
+
+la $t1, is_equal
+beqz $t0, strings_is_equal
+nop
+
+addiu $t1, $t1, 16	# set string pointer to is_not_equal
+
+strings_is_equal:
+move $a0, $t1		# move string pointer to argument
+li $v0, 4
+syscall
+
+# epilog
+lw $ra, ($sp)
+nop
+addiu $sp, $sp, 4	# pop $ra from stack
+
+jr $ra
+nop
+
+.data
+buffer1: .space 100
+buffer2: .space 100
+is_equal: .asciiz "String is equal"
+is_not_equal: .asciiz "String is not equal"
+
+
+.text
+# $a0 = buffer address
+# $a1 = buffer length
+.globl ask_for_string
+ask_for_string:
+# prolog
+
+# code
+move $t0, $a0	# preserve buffer address
+li $v0, 4	# print string
+la $a0, ask_for_string_prompt
+nop
+syscall
+
+li $v0, 8 	# read string
+move $a0, $t0	# move buffer address
+syscall
+
+# epilog
+jr $ra 
+nop
+.data
+ask_for_string_prompt: .asciiz "\nPlease write string: \n > "
+
+
+.text
+# $a0 = first string buffer address
+# $a1 = second string buffer address
+# $s0 = first char
+# $s1 = second char
+# $v0 = result (0 = equal, 1 = not equal)
+.globl compare_strings
+compare_strings:
+
+# prolog
+subu $sp, $sp, 4	# stack push
+sw $s0, ($sp)
+
+subu $sp, $sp, 4 	# stack push
+sw $s1, ($sp)
+
+move $t0, $a0		# move first string buffer address to $t0
+move $t1, $a1		# move second string buffer address to $t1
+
+# code
+li $v0, 0		# set default return value
+
+compare_strings_loop:
+lbu $s0, ($t0)
+nop
+lbu $s1, ($t1)
+nop
+
+bne $s0, $s1, compare_strings_loop_not_equal
+nop
+
+beq $s0, 0x0a, compare_strings_loop_end
+nop 
+
+addiu $t0, $t0, 1	# increment first string address
+addiu $t1, $t1, 1	# increment second string address
+
+j compare_strings_loop
+nop
+compare_strings_loop_not_equal:
+li $v0, 1
+
+compare_strings_loop_end:
+
+# epilog
+lw $s1, ($sp)		# stack pop
+addiu $sp, $sp, 4
+
+lw $s0, ($sp)		# stack pop
+addiu $sp, $sp, 4	
+
+jr $ra
+nop
+.data
+
+
